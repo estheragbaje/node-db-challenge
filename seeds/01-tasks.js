436ba2d3d18482aaff0c53403969c78d72d83bdd
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-08 11:59:17 +0100

    Seeded task file

--- seeds/01-tasks.js
+++ seeds/01-tasks.js
@@ -1,13 +1,28 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+  // Deletes ALL existing entries
+  return knex("tasks")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("tasks").insert([
+        {
+          task_desc: "buy things",
+          notes: "extra",
+          completed: 0,
+          project_id: 1
+        },
+        {
+          task_desc: "call clients",
+          notes: "extra",
+          completed: 1,
+          project_id: 3
+        },
+        {
+          task_desc: "go shopping",
+          notes: "extra",
+          completed: 1,
+          project_id: 2
+        }
       ]);
     });
 };
