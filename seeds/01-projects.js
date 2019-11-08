6fb881c53ddb17a9d536865fb72c2840508b856b
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-08 11:53:46 +0100

    Seeded project file

--- seeds/01-projects.js
+++ seeds/01-projects.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+  // Deletes ALL existing entries
+  return knex("projects")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("projects").insert([
+        { project_name: "Cascading Giants" },
+        { project_name: "Online Training" },
+        { project_name: "Lambda School" }
       ]);
     });
 };
