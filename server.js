bea4d8da5e3518405d9b15a8f48f1e09a064dd31
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-08 09:37:45 +0100

    Created server.js file and populated it

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("<h4>Hello from Project Management App</h4>");
+});
+
+module.exports = server;
